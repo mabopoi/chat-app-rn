aadface8974e30a0919a5356f323a022d9e9ee67
unknown <mabopoi@gmail.com>
2020-12-12 03:02:22 -0300

    Chat done w/o styles

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,22 +1,37 @@
-import React, { useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
 import io from 'socket.io-client';
 
 const ChatScreen = () => {
-  const socket = io('localhost:3000');
-  let msg = 'hello';
+  const [message, setMessage] = useState('');
+  const [chatMessages, setChatMessages] = useState([]);
+  const socketRef = useRef();
+
   useEffect(() => {
-    socket.on('chat msg', () => {
-      console.log('new msg');
-    });
+    socketRef.current = io('localhost:3000');
+    socketRef.current.on('chat msg', (msg) =>
+      setChatMessages((chatMessages) => [...chatMessages, msg])
+    );
   }, []);
+
+  const sendMessage = () => {
+    socketRef.current.emit('chat msg', message);
+    setMessage('');
+  };
 
   return (
     <View>
-      <Text>Testing Socket.io</Text>
-      <Button title='send' onPress={() => socket.emit('chat msg', msg)} />
+      {chatMessages.map((msg) => (
+        <Text>{msg}</Text>
+      ))}
+      <TextInput value={message} onChangeText={(text) => setMessage(text)} />
+      <View>
+        <Button title='send' onPress={sendMessage} />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default ChatScreen;
