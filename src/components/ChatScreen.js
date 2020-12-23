91ff78b28b6e7a2efd0ec9a66b90fefb5dc78833
unknown <mabopoi@gmail.com>
2020-12-23 02:44:15 -0300

    Starting chat UI

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TextInput,
+  FlatList,
+} from 'react-native';
 import io from 'socket.io-client';
 
 const ChatScreen = () => {
@@ -20,18 +27,68 @@ const ChatScreen = () => {
   };
 
   return (
-    <View>
-      {chatMessages.map((msg) => (
-        <Text>{msg}</Text>
-      ))}
-      <TextInput value={message} onChangeText={(text) => setMessage(text)} />
-      <View>
-        <Button title='send' onPress={sendMessage} />
+    <View style={styles.container}>
+      <View style={styles.messagesContainer}>
+        <FlatList
+          data={chatMessages}
+          renderItem={(msg) => (
+            <View style={styles.msgBackground}>
+              <Text style={styles.msg}>{msg.item}</Text>
+            </View>
+          )}
+          keyExtractor={(item, index) => index.toString()}
+          ListEmptyComponent={() => (
+            <Text style={styles.msg}>There aren't messages yet</Text>
+          )}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={message}
+          onChangeText={(text) => setMessage(text)}
+        />
+        <View>
+          <Button title='send' onPress={sendMessage} />
+        </View>
       </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  messagesContainer: {
+    height: 400,
+    width: 400,
+    borderColor: '#000',
+    borderWidth: 2,
+    alignItems: 'center',
+  },
+  msgBackground: {
+    backgroundColor: '#00C6D6',
+    width: 150,
+    borderRadius: 10,
+    margin: 7,
+  },
+  msg: {
+    fontSize: 16,
+    marginLeft: 5,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  input: {
+    height: 35,
+    borderColor: '#000',
+    borderWidth: 2,
+  },
+});
 
 export default ChatScreen;
